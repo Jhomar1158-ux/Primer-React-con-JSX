3eec0ca3f8dc45c487b8c5b4588421de53b36f9e
Jhomar1158-ux <jhomarcristianelias@gmail.com>
2022-05-11 11:55:12 -0500

    first commit

---
+++ src/componentes/ListaDeTareas.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ListaDeTareas = () =>{
+    return(
+        <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+        </ul>
+    )
+}
+
+
+export default ListaDeTareas;
